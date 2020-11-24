1efc288d1ca592bd1064b6a21fd7930908b82705
rabah babaci <4rabah@gmail.com>
2020-11-24 11:46:45 -0800

    install normalize.css

--- src/components/common/Navigation.js
+++ src/components/common/Navigation.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
+import { FaUserCircle } from 'react-icons/fa';
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  border: 1px solid red;
+  padding: 15px;
+
+  svg {
+    /* vertical-align: middle; */
+  }
+`;
 
 export default function navigation() {
   return (
-    <div className="nav-wrapper">
-      <h1>Navigation Component</h1>
-    </div>
+    <Wrapper>
+      <FaUserCircle size="3em" />
+    </Wrapper>
   );
 }
