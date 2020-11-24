d1d058221934f205b9dbadefad534fb809c04897
rabah babaci <4rabah@gmail.com>
2020-11-24 13:09:59 -0800

    make the nav-bar fixed

--- src/components/common/Navigation.js
+++ src/components/common/Navigation.js
@@ -2,60 +2,90 @@ import React from 'react';
 import styled from 'styled-components';
 import { FaUserCircle } from 'react-icons/fa';
 import { AiOutlineShoppingCart } from 'react-icons/ai';
-import { BiSearchAlt } from 'react-icons/bi';
+import { BiSearchAlt, BiChevronDown } from 'react-icons/bi';
 
 const Wrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  padding: 15px;
-  background-color: #eff5f3;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  background-color: #deebe6;
 
-  .search-bar-wrapper {
-    position: relative;
-
-    input {
-      font-size: 15px;
-      width: 300px;
-      padding: 5px 10px;
-      border: none;
-      color: black;
-    }
-    svg {
-      position: absolute;
-      right: 5px;
-      top: 6px;
-    }
-  }
-
-  .cart {
+  main {
     display: flex;
+    justify-content: space-between;
     align-items: center;
+    padding: 15px;
 
-    span {
-      margin-left: 5px;
-      font-weight: 500;
-      font-size: 16px;
+    .search-bar-wrapper {
+      position: relative;
+      top: 30px;
+
+      input {
+        font-size: 15px;
+        width: 350px;
+        padding: 6px 10px;
+        border: none;
+        color: black;
+      }
+      svg {
+        position: absolute;
+        right: 5px;
+        top: 7px;
+      }
+    }
+
+    .cart {
+      display: flex;
+      align-items: center;
+
+      span {
+        margin-left: 5px;
+        font-weight: 500;
+        font-size: 16px;
+      }
     }
   }
 
-  svg {
-    /* vertical-align: middle; */
+  .navigation {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    padding: 15px 8px 15px 0;
+
+    section {
+      display: flex;
+      align-items: center;
+
+      span {
+        margin-left: 5px;
+      }
+    }
   }
 `;
 
-export default function navigation() {
+export default function navigation({ userInfo }) {
   return (
     <Wrapper>
-      <span>LOGO</span>
+      <main>
+        <span>LOGO</span>
 
-      <div className="search-bar-wrapper">
-        <input type="text" placeholder="Search MarketPlace" />
-        <BiSearchAlt size="1.6em" />
-      </div>
-      {/* <FaUserCircle size="2.5em" color="#607d8b" /> */}
-      <div className="cart">
-        <AiOutlineShoppingCart size="2.5em" color="#4a626e" />
-        <span>Cart</span>
+        <div className="search-bar-wrapper">
+          <input type="text" placeholder="Search MarketPlace" />
+          <BiSearchAlt size="1.6em" />
+        </div>
+
+        <div className="cart">
+          <AiOutlineShoppingCart size="2.5em" color="#4a626e" />
+          <span>Cart</span>
+        </div>
+      </main>
+
+      <div className="navigation">
+        <section>
+          <FaUserCircle size="2.5em" color="#607d8b" />
+          <span>Hi, {userInfo.name.split(' ')[0]}</span>
+          <BiChevronDown size="2em" />
+        </section>
       </div>
     </Wrapper>
   );
--- src/components/pages/Home/RenderHomePage.js
+++ src/components/pages/Home/RenderHomePage.js
@@ -1,36 +1,45 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 import { Navigation, Button } from '../../common';
+
+const Wrapper = styled.div`
+  .main-content {
+    margin-top: 150px;
+  }
+`;
 
 function RenderHomePage(props) {
   const { userInfo, authService } = props;
   return (
-    <div>
-      <Navigation />
+    <Wrapper>
+      <Navigation userInfo={userInfo} />
 
-      <h1>Hi {userInfo.name} Welcome to Labs Basic SPA</h1>
-      <div>
-        <p>
-          This is an example of a common example of how we'd like for you to
-          approach components.
-        </p>
-        <p>
-          <Link to="/profile-list">Profiles Example</Link>
-        </p>
-        <p>
-          <Link to="/example-list">Example List of Items</Link>
-        </p>
-        <p>
-          <Link to="/datavis">Data Visualizations Example</Link>
-        </p>
-        <p>
-          <Button
-            handleClick={() => authService.logout()}
-            buttonText="Logout"
-          />
-        </p>
+      <div className="main-content">
+        <h1>Hi {userInfo.name} Welcome to Labs Basic SPA</h1>
+        <div>
+          <p>
+            This is an example of a common example of how we'd like for you to
+            approach components.
+          </p>
+          <p>
+            <Link to="/profile-list">Profiles Example</Link>
+          </p>
+          <p>
+            <Link to="/example-list">Example List of Items</Link>
+          </p>
+          <p>
+            <Link to="/datavis">Data Visualizations Example</Link>
+          </p>
+          <p>
+            <Button
+              handleClick={() => authService.logout()}
+              buttonText="Logout"
+            />
+          </p>
+        </div>
       </div>
-    </div>
+    </Wrapper>
   );
 }
 export default RenderHomePage;
