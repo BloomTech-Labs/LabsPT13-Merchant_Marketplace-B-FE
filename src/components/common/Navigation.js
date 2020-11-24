23783e4fa6b748fcbf2c235e0a993e82202e3422
rabah babaci <4rabah@gmail.com>
2020-11-24 11:34:30 -0800

    define the navigation component

---
+++ src/components/common/Navigation.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function navigation() {
+  return (
+    <div className="navigation-wrapper">
+      <h1>Navigation Component</h1>
+    </div>
+  );
+}
--- src/components/pages/Home/RenderHomePage.js
+++ src/components/pages/Home/RenderHomePage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from '../../common';
+import { Navigation, Button } from '../../common';
 
 function RenderHomePage(props) {
   const { userInfo, authService } = props;
   return (
     <div>
+      <Navigation />
+
       <h1>Hi {userInfo.name} Welcome to Labs Basic SPA</h1>
       <div>
         <p>
