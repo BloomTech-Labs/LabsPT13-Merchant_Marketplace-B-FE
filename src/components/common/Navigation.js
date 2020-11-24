5ff44c4c6e037a40ff16c24aa106a813da3bb742
rabah babaci <4rabah@gmail.com>
2020-11-24 12:29:00 -0800

    create a basic search bar

--- src/components/common/Navigation.js
+++ src/components/common/Navigation.js
@@ -2,12 +2,30 @@ import React from 'react';
 import styled from 'styled-components';
 import { FaUserCircle } from 'react-icons/fa';
 import { AiOutlineShoppingCart } from 'react-icons/ai';
+import { BiSearchAlt } from 'react-icons/bi';
 
 const Wrapper = styled.div`
   display: flex;
-  justify-content: flex-end;
-  border: 1px solid red;
+  justify-content: space-between;
   padding: 15px;
+  background-color: #eff5f3;
+
+  .search-bar-wrapper {
+    position: relative;
+
+    input {
+      font-size: 15px;
+      width: 300px;
+      padding: 5px 10px;
+      border: none;
+      color: black;
+    }
+    svg {
+      position: absolute;
+      right: 5px;
+      top: 6px;
+    }
+  }
 
   .cart {
     display: flex;
@@ -28,6 +46,12 @@ const Wrapper = styled.div`
 export default function navigation() {
   return (
     <Wrapper>
+      <span>LOGO</span>
+
+      <div className="search-bar-wrapper">
+        <input type="text" placeholder="Search MarketPlace" />
+        <BiSearchAlt size="1.6em" />
+      </div>
       {/* <FaUserCircle size="2.5em" color="#607d8b" /> */}
       <div className="cart">
         <AiOutlineShoppingCart size="2.5em" color="#4a626e" />
