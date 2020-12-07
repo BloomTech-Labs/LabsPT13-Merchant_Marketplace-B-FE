490b3d04f484950ecde40f38e8d3650f9ee25ffc
rabah babaci <4rabah@gmail.com>
2020-12-07 14:32:07 -0800

    setup the product page wire frame

--- src/components/pages/Product/RenderProduct.js
+++ src/components/pages/Product/RenderProduct.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const Wrapper = styled.div`
+  .details {
+    height: 100%;
+    width: 400px;
+    position: fixed;
+    top: 0;
+    right: 0;
+    background-color: #e7e7e7;
+    overflow-x: hidden;
+  }
+
+  .main {
+    margin-right: 400px;
+  }
+`;
+
 const RenderProduct = ({ product }) => {
   return (
-    <div>
-      <h1>Product Page</h1>
-    </div>
+    <Wrapper>
+      <div className="details"></div>
+
+      <div className="main">
+        <h1>Product Page</h1>
+      </div>
+    </Wrapper>
   );
 };
 
