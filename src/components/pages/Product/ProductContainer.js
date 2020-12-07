d3b286ba2466cdf460382bdec646538063e81cf9
rabah babaci <4rabah@gmail.com>
2020-12-07 14:17:37 -0800

    fetch for product info and pass it to renderProduct

--- src/components/pages/Product/ProductContainer.js
+++ src/components/pages/Product/ProductContainer.js
@@ -1,11 +1,41 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import { useOktaAuth } from '@okta/okta-react';
+import { useLocation, useParams } from 'react-router-dom';
+import { getProductById } from '../../../api';
 import RenderProduct from './RenderProduct';
+import { NavigationBar } from '../../common';
 
 export default function ProductContainer() {
+  const { authState } = useOktaAuth();
+  const { product } = useLocation();
+  const [productInfo, setProductInfo] = useState(product);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const product = await getProductById(id, authState);
+        setProductInfo(product);
+      } catch (error) {
+        console.error(error);
+        // Be sure to add functionality that displays errors to your UI here.
+        // We want our users to know whether something has gone wrong with our request.
+      }
+    };
+
+    // only fetch if product info is not passed via Link
+    if (!productInfo) fetchProduct();
+  }, []);
+
   return (
-    <div>
-      <RenderProduct />
-    </div>
+    <>
+      <NavigationBar />
+
+      {productInfo ? (
+        <RenderProduct product={productInfo} />
+      ) : (
+        <span>...Fetching product info</span>
+      )}
+    </>
   );
 }
