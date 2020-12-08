e17a7c9ea8cfb080960bcd5531e262e82e8a7120
rabah babaci <4rabah@gmail.com>
2020-12-08 11:30:24 -0800

    position the carousel container

--- src/components/pages/Product/RenderProduct.js
+++ src/components/pages/Product/RenderProduct.js
@@ -1,34 +1,65 @@
 import React from 'react';
 import styled from 'styled-components';
+import ImageGallery from 'react-image-gallery';
 
 const Wrapper = styled.div`
   display: flex;
-  justify-content: flex-end;
+  flex-direction: column;
+  background-color: #f7f7f7;
 
   .details {
-    height: 100%;
-    width: 400px;
-    background-color: #e7e7e7;
+    height: 110vh;
+    width: 360px;
+    align-self: flex-end;
   }
 
   .main {
     position: fixed;
-    background-color: orange;
+    align-self: flex-start;
+    width: calc(100% - 360px);
     height: 100%;
-    width: calc(100% - 400px);
-    margin-right: 400px;
+    display: flex;
+
+    .carousel-wrapper {
+      width: 100%;
+      align-self: center;
+
+      svg {
+        height: 100px;
+        width: 45px;
+      }
+    }
   }
 `;
 
 const RenderProduct = ({ product }) => {
+  const images = [
+    {
+      original: 'https://picsum.photos/id/1018/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1018/250/150/',
+    },
+    {
+      original: 'https://picsum.photos/id/1015/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1015/250/150/',
+    },
+    {
+      original: 'https://picsum.photos/id/1019/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1019/250/150/',
+    },
+  ];
+
   return (
     <Wrapper>
-      <div className="details">
-        <h1>Details</h1>
+      <div className="main">
+        <ImageGallery
+          items={images}
+          showPlayButton={false}
+          additionalClass="carousel-wrapper"
+        />
       </div>
 
-      <div className="main">
-        <div className="carousel-wrapper"></div>
+      <div className="details">
+        <h1>Details</h1>
       </div>
     </Wrapper>
   );
