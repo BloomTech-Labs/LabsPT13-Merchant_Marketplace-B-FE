6f73bc7a00e065aa8a6aad3f907ad64e044b4804
rabah babaci <4rabah@gmail.com>
2020-12-14 18:33:23 -0800

    setup the file structure for create product page

---
+++ src/components/pages/CreateProduct/CreateProductContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import RenderCreateProduct from './RenderCreateProduct';
+
+export default function CreateProductContainer() {
+  return (
+    <div>
+      <RenderCreateProduct />
+    </div>
+  );
+}
