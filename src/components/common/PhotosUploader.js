9dec1ba6ed92405259e15f1944f5a72cc821f851
rabah babaci <4rabah@gmail.com>
2020-12-15 22:02:49 -0800

    create the Photos Uploader component

---
+++ src/components/common/PhotosUploader.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import ImageUploader from 'react-images-upload';
+
+export default function PhotosUploader() {
+  const [photos, setPhotos] = useState([]);
+
+  const onDrop = (photoFiles, photoDataURLs) => {
+    setPhotos([...photos, photoFiles]);
+  };
+
+  console.log(photos);
+
+  return (
+    <ImageUploader
+      withIcon={false}
+      withPreview={true}
+      buttonText="Add Photos"
+      onChange={onDrop}
+      imgExtension={['.jpg', '.gif', '.png', '.gif']}
+      maxFileSize={5242880}
+    />
+  );
+}
