bac73ad4eb8843519a7838c061bc939d1b3fa0ef
rabah babaci <4rabah@gmail.com>
2020-12-15 21:00:04 -0800

    install react-images-upload

--- src/components/pages/CreateProduct/CreateProductContainer.js
+++ src/components/pages/CreateProduct/CreateProductContainer.js
@@ -2,11 +2,11 @@ import React from 'react';
 import RenderCreateProduct from './RenderCreateProduct';
 
 export default function CreateProductContainer() {
-  const sellerInfo = JSON.parse(window.localStorage.getItem('user'));
+  const userInfo = JSON.parse(window.localStorage.getItem('user'));
 
   return (
     <div>
-      <RenderCreateProduct sellerInfo={sellerInfo} />
+      <RenderCreateProduct userInfo={userInfo} />
     </div>
   );
 }
