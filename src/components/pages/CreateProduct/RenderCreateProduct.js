c1e320574c4ec6997da4a3a72cda0875a5ea3db6
rabah babaci <4rabah@gmail.com>
2020-12-15 21:00:04 -0800

    install react-images-upload

--- src/components/pages/CreateProduct/RenderCreateProduct.js
+++ src/components/pages/CreateProduct/RenderCreateProduct.js
@@ -4,20 +4,53 @@ import styled from 'styled-components';
 
 import { ArrowLeftOutlined } from '@ant-design/icons';
 const Wrapper = styled.div`
+  height: 100vh;
+
   .form-wrapper {
-    border: 1px solid red;
-    max-width: 800px;
-    height: 100vh;
+    max-width: 700px;
     margin: auto;
     padding: 15px;
+    height: 100%;
+    border: 1px solid;
 
-    h1 {
-      font-weight: 600;
+    .user-info {
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+      height: 70px;
+      margin-bottom: 15px;
+
+      img {
+        height: 50px;
+        width: 50px;
+        border-radius: 50%;
+        object-fit: cover;
+      }
+
+      .info {
+        display: flex;
+        flex-direction: column;
+        margin-left: 10px;
+        font-weight: bold;
+        font-size: 16px;
+
+        span {
+          font-weight: normal;
+          font-size: 14px;
+        }
+      }
+    }
+
+    .photos-upload {
+      border: 1px solid;
+      height: 130px;
+      border-radius: 3px;
+      cursor: pointer;
     }
   }
 `;
 
-export default function RenderCreateProduct({ sellerInfo }) {
+export default function RenderCreateProduct({ userInfo }) {
   return (
     <Wrapper>
       <Link to="/">
@@ -25,7 +58,19 @@ export default function RenderCreateProduct({ sellerInfo }) {
       </Link>
 
       <div className="form-wrapper">
-        <h1>Item For Sale</h1>
+        <section className="user-info">
+          <img
+            src="https://www.mercurynews.com/wp-content/uploads/2020/04/slowstreets417.jpg"
+            alt="seller avatar"
+          />
+          <section className="info">
+            {userInfo.name}
+            <span style={{ color: 'gray' }}>Listing to Marketplace</span>
+          </section>
+        </section>
+
+        <span>Photos - 0/5 - You can add up to 5 photos.</span>
+        <section className="photos-upload"></section>
       </div>
     </Wrapper>
   );
