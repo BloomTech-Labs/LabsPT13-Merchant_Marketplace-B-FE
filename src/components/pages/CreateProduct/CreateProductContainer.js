2f3260effd26b236cf497cb22a3b799c233da7aa
rabah babaci <4rabah@gmail.com>
2020-12-17 12:25:03 -0800

    create all the form handlers

--- src/components/pages/CreateProduct/CreateProductContainer.js
+++ src/components/pages/CreateProduct/CreateProductContainer.js
@@ -1,35 +1,58 @@
 import React, { useState } from 'react';
 import RenderCreateProduct from './RenderCreateProduct';
 
+const initialState = {
+  seller_id: null,
+  title: '',
+  description: '',
+  category: '',
+  tags: ['Fridge', 'Music'],
+  price: '',
+  brand: '',
+  condition: '',
+  delivery_method: '',
+};
+
 export default function CreateProductContainer() {
   const userInfo = JSON.parse(window.localStorage.getItem('user'));
-
-  const [formInfo, setFormInfo] = useState({
-    images: [],
-    seller_id: userInfo.sub,
-    title: '',
-    description: '',
-    category: '',
-    tags: '',
-    price: '',
-    brand: '',
-    condition: '',
-    delivery_method: '',
-  });
+  const [formInfo, setFormInfo] = useState(initialState);
+  const [images, setImages] = useState([]);
+  const [newTag, setNewTag] = useState('');
 
   const onDropImages = (imageFiles, photoDataURLs) => {
-    setFormInfo({ ...formInfo, images: imageFiles });
+    setImages(imageFiles);
   };
 
   const handleChange = (name, value) => {
     setFormInfo({ ...formInfo, [name]: value });
   };
 
-  const handleSubmit = () => {
-    console.log('Submit form info');
+  const handleTagChange = e => setNewTag(e.target.value);
+
+  const addTag = tag => {
+    setFormInfo({ ...formInfo, tags: [...formInfo.tags, tag] });
   };
 
-  console.log(formInfo.description);
+  const removeTag = tag => {
+    setFormInfo({ ...formInfo, tags: formInfo.tags.filter(t => t !== tag) });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setFormInfo(initialState);
+    setImages([]);
+
+    console.log(formInfo);
+    console.log(images);
+  };
+
+  const handleKeyPress = e => {
+    if (e.which === 13) {
+      e.preventDefault();
+    }
+  };
+
+  console.log(newTag);
 
   return (
     <div>
@@ -39,6 +62,11 @@ export default function CreateProductContainer() {
         onDropImages={onDropImages}
         handleChange={handleChange}
         handleSubmit={handleSubmit}
+        handleKeyPress={handleKeyPress}
+        newTag={newTag}
+        handleTagChange={handleTagChange}
+        addTag={addTag}
+        removeTag={removeTag}
       />
     </div>
   );
