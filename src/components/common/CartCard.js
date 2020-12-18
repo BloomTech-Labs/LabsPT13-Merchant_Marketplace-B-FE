69f6f78f87992debf4f6fb874b3ca18317b69731
Bola Inc <fngafua2023@gmail.com>
2020-12-18 00:54:40 -0700

    Add elements to the card and defined the height and width of the card using styled component

--- src/components/common/CartCard.js
+++ src/components/common/CartCard.js
@@ -1,3 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+
+const Card = styled.div`
+  height: 300px;
+  width: 260px;
+`;
+
+export default function CartCard({ product }) {
+  return (
+    <Link>
+      <Card>
+        <img src={product.img} alt="market product" />
+
+        <div className="productDetails">
+          <h4>${product.title}</h4>
+          <h3>{product.price}</h3>
+          <h3> Quantity: 1</h3>
+        </div>
+      </Card>
+    </Link>
+  );
+}
