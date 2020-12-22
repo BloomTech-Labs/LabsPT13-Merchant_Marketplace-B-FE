1aac0055a04a69877618fd41a033193e2eea2c35
Devin Cooper <37602228+DevinDCooper@users.noreply.github.com>
2020-12-21 19:10:05 -0800

    added logic to components from old PR

---
+++ src/components/pages/Wishlist/RenderWishlist.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+import WishlistCard from '../../common/WishlistCard';
+import { NavigationBar } from '../../common';
+
+const Wrapper = styled.div``;
+
+const RenderWishlist = () => {
+  return (
+    <Wrapper>
+      <h1 style={{ marginTop: '160px' }}>Wishlist Page</h1>
+      <WishlistCard />
+    </Wrapper>
+  );
+};
+export default RenderWishlist;
