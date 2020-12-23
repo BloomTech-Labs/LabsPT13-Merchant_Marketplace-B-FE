a6c43634569c95dadfd2154b827a9f6619df564b
rabah babaci <4rabah@gmail.com>
2020-12-23 10:21:13 -0800

    configure the redux store

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import persistState from 'redux-localstorage';
+import rootReducer from './state/reducers';
 import App from './App';
 import 'normalize.css';
 import 'antd/dist/antd.less';
 import './index.css';
 
+const enhancer = compose(applyMiddleware(thunk), persistState());
+const store = createStore(rootReducer, enhancer);
+
 ReactDOM.render(
-  <Router>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Router>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
