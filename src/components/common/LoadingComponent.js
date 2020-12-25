8e0c049ec8eaee153527e993071e69658ab8c261
rabah babaci <4rabah@gmail.com>
2020-12-25 12:14:14 -0800

    adjust the LoadingComponent position

--- src/components/common/LoadingComponent.js
+++ src/components/common/LoadingComponent.js
@@ -1,26 +1,17 @@
 import React from 'react';
 import LoadingOverlay from 'react-loading-overlay';
 import ScaleLoader from 'react-spinners/ScaleLoader';
-import styled from 'styled-components';
-
-const Wrapper = styled.div`
-  ._loading_overlay_content {
-    margin-top: calc(50vh - 60px);
-    color: #f5f4f4;
-  }
-`;
 
 const LoadingComponent = ({ active, children, message }) => {
   return (
-    <Wrapper>
-      <LoadingOverlay
-        active={active}
-        spinner={<ScaleLoader color="#36D7B7" />}
-        text={message}
-      >
-        {children}
-      </LoadingOverlay>
-    </Wrapper>
+    <LoadingOverlay
+      active={active}
+      spinner={<ScaleLoader color="#36D7B7" />}
+      text={message}
+      fadeSpeed={300}
+    >
+      {children}
+    </LoadingOverlay>
   );
 };
 
