a1a7025fb10b5324855adb78786a432646af837d
Bola Inc <fngafua2023@gmail.com>
2020-12-31 02:04:31 -0700

    Create the order summary sub-section

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -9,7 +9,8 @@ import ImagesGallery from './ImagesGallery';
 import MarketplaceFeed from './MarketplaceFeed';
 import ProductCard from './ProductCard';
 import CartCard from './CartCard';
-import Payments from './Payments'
+import Payments from './Payments';
+import OrderSummary from './OrderSummary';
 // notice we're building out a 'package' of reusables here and exporting them as an object of component properties.
 // to use this, simply `import {foo, bar, baz} from '<path-to-this-directory>/ReusableComponents';`
 export {
@@ -25,4 +26,5 @@ export {
   ProductCard,
   CartCard,
   Payments,
+  OrderSummary,
 };
