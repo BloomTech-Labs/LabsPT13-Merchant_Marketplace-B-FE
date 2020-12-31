0f6da0be13f207306d9a2b6d7dad9df0396624e0
Bola Inc <fngafua2023@gmail.com>
2020-12-31 01:47:51 -0700

    Define varius section of the checkout page

---
+++ src/components/pages/Checkout/RenderCheckout.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+import { CartCard } from '../../common';
+
+
+const Wrapper = styled.div`
+  
+`;
+const InfoWrapper = styled.div`
+  display: flex;
+  margin: 0rem 4rem 0rem 4rem;
+  
+`;
+const OrderSummaryWrapper = styled.div`
+
+`;
+
+const RenderCheckout = () => {
+  return (
+    <Wrapper>
+      <InfoWrapper>
+      
+      </InfoWrapper>
+      <OrderSummaryWrapper>
+
+      </OrderSummaryWrapper>
+      
+      
+    </Wrapper>
+  );
+};
+
+export default RenderCart;
