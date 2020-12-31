ed9993b2bcc13675e4d8e2d42f24d05eade4a9b5
Bola Inc <fngafua2023@gmail.com>
2020-12-31 02:30:23 -0700

    Create file for checkout contact info

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -11,6 +11,7 @@ import ProductCard from './ProductCard';
 import CartCard from './CartCard';
 import Payments from './Payments';
 import OrderSummary from './OrderSummary';
+import ContactInfo from './ContactInfo';
 // notice we're building out a 'package' of reusables here and exporting them as an object of component properties.
 // to use this, simply `import {foo, bar, baz} from '<path-to-this-directory>/ReusableComponents';`
 export {
@@ -27,4 +28,5 @@ export {
   CartCard,
   Payments,
   OrderSummary,
+  ContactInfo,
 };
