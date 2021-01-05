cf481dbe70811b24ec98854b0b8247346cc36940
rabah babaci <4rabah@gmail.com>
2021-01-04 19:00:37 -0800

    define the purchase history page

---
+++ src/components/pages/PurchaseHistory.js/PurchaseHistoryContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import RenderPurchaseHistory from './RenderPurchaseHistory';
+
+export default function PurchaseHistoryContainer() {
+  return (
+    <div>
+      <RenderPurchaseHistory />
+    </div>
+  );
+}
