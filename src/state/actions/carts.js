6ea430ec5a94489902c13266e3f69bf07094ef5a
Bola Inc <fngafua2023@gmail.com>
2021-01-05 22:24:26 -0700

    Add action to add item to the cart

--- src/state/actions/carts.js
+++ src/state/actions/carts.js
@@ -1,5 +1,6 @@
 import { getCartItems } from '../../api';
 import { removeCartItem } from '../../api';
+import { addCartItem } from '../../api';
 
 export const fetchCart = (profile_id,authState) => dispatch => {
   try {
@@ -28,6 +29,22 @@ export const removeFromCart = (profile_id, product_id, authState) => dispatch =>
   } catch (err) {
     dispatch({
       type: 'ERROR_REMOVING_ITEM',
+      payload: 'Request failed, please try again!',
+    });
+    console.error(err)
+  }
+}
+
+export const addToCart = (profile_id, product_id, authState) => dispatch => {
+  try {
+    console.log('ADDING ITEM TO THE CART')
+    dispatch({ type: 'ADD_TO_CART'});
+    addCartItem(profile_id, product_id, authState).then(cart => {
+      dispatch({ type: 'ITEM_ADDED', payload: cart})
+    })
+  } catch (err) {
+    dispatch({
+      type: 'ERROR_ADDING_ITEM',
       payload: 'Request failed, please try again!',
     });
     console.error(err)
