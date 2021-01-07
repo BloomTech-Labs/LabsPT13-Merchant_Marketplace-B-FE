9913407cf4186ab780cd0f7883dcc7fd754f0e90
Bola Inc <fngafua2023@gmail.com>
2021-01-06 20:53:00 -0700

    fixing bugs

--- src/state/actions/index.js
+++ src/state/actions/index.js
@@ -1,19 +1,3 @@
-// import all of your actions into this file, and export them back out.
-// This allows for the simplification of flow when importing actions into your components throughout your app.
-
-// Actions should be focused to a single purpose. 
-// You can have multiple action creators per file if it makes sense to the purpose those action creators are serving. 
-// Declare action TYPES at the top of the file
-import { fetchCart } from './carts';
-import { removeFromCart } from './carts';
-import { addToCart } from './carts';
-
-export {
-    fetchCart,
-    removeFromCart,
-    addToCart,
-}
-
 // Actions should be focused to a single purpose.
 // You can have multiple action creators per file if it makes sense to the purpose those action creators are serving.
 // Declare action TYPES at the top of the file
